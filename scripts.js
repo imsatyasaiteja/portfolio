afbc66a6421e29848ace321a41b5318879ef5393
Teja <imsatyasaiteja@gmail.com>
2022-08-02 12:34:08 +0530

    My first commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// Navigation bar : 
+
+window.onscroll = function() {myFunction()};
+function myFunction() 
+{
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+// Fade out on Scroll :
+$(window).scroll(function () {
+  var scrollVar = $(window).scrollTop();
+  $(".section_1").css("opacity", 1 - scrollVar / 500);
+})
